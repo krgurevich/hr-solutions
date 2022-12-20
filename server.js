6bed572a8cc7592d2c5293e8b3fec34504e336f6
krgurevich <kananyan@optonline.net>
2022-12-20 13:09:41 -0500

    modified server.js to fix null issue in the update role

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const connection = require("./config/connection");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
-// Launch application
+/* === || Launch Application - Main Menu || === */
 
 const mainMenu = () => {
   inquirer
@@ -32,7 +32,7 @@ const mainMenu = () => {
           addEmployee();
           break;
         case "Update Employee Role":
-          updateEmployee();
+          updateRole();
           break;
         case "View All Roles":
           viewAllRoles();
@@ -54,10 +54,13 @@ const mainMenu = () => {
 };
 
 /* === || Employee, Dept and Role Arrays || === */
+
 // Department Array
-const deptIdArray = [];
-const deptNameArray = [];
+let deptIdArray = [];
+let deptNameArray = [];
 function deptChoices() {
+  deptIdArray = [];
+  deptNameArray = [];
   connection.query("SELECT * FROM departments", function (error, response) {
     if (error) throw error;
     for (let i = 0; i < response.length; i++) {
@@ -68,22 +71,26 @@ function deptChoices() {
   return { id: deptIdArray, name: deptNameArray };
 }
 // Employee Array
-const empFirstName = [];
-const empLastName = [];
+let empIdArray = [];
+let empNameArray = [];
 function empChoices() {
+  empIdArray = [];
+  empNameArray = [];
   connection.query("SELECT * FROM employees", function (error, response) {
     if (error) throw error;
     for (let i = 0; i < response.length; i++) {
-      empFirstName.push(response[i].first_name);
-      empLastName.push(response[i].last_name);
+      empIdArray.push(response[i].employee_id);
+      empNameArray.push(response[i].first_name + " " + response[i].last_name);
     }
   });
-  return { firstName: empFirstName, lastName: empLastName };
+  return { id: empIdArray, name: empNameArray };
 }
 // Role Array
-const roleIdArray = [];
-const roleTitleArray = [];
+let roleIdArray = [];
+let roleTitleArray = [];
 function roleChoices() {
+  roleIdArray = [];
+  roleTitleArray = [];
   connection.query("SELECT * FROM roles", function (error, response) {
     if (error) throw error;
     for (let i = 0; i < response.length; i++) {
@@ -94,24 +101,28 @@ function roleChoices() {
   return { id: roleIdArray, title: roleTitleArray };
 }
 // //Manager Array
-const managerIdArray = [];
-const managerNameArray = [];
+let managerIdArray = [];
+let managerNameArray = [];
 function managerChoices() {
+  managerIdArray = [];
+  managerNameArray = [];
   connection.query(
     `SELECT * FROM employees WHERE manager_id IS NULL`,
     function (error, response) {
       if (error) throw error;
       for (let i = 0; i < response.length; i++) {
         managerIdArray.push(response[i].employee_id);
-        managerNameArray.push(response[i].first_name + response[i].last_name);
+        managerNameArray.push(
+          response[i].first_name + " " + response[i].last_name
+        );
       }
+      managerNameArray.push("None");
     }
   );
   return { id: managerIdArray, name: managerNameArray };
 }
 
 /* === || View All Employees || === */
-
 const viewAllEmployees = () => {
   const sql = `
   SELECT  a.employee_id,
@@ -122,7 +133,7 @@ const viewAllEmployees = () => {
         b.salary,
         CASE WHEN a.manager_id IS NULL THEN NULL ELSE concat(d.first_name," ",d.last_name) END AS manager_name
 FROM employees a
-LEFT JOIN roles b ON a.employee_id = b.role_id
+LEFT JOIN roles b ON a.role_id = b.role_id
 LEFT JOIN departments c ON c.department_id = b.department_id
 LEFT JOIN (
         SELECT c.department_name, 
@@ -169,28 +180,27 @@ const addEmployee = () => {
       },
     ])
     .then(function (answer) {
-      roleChoices();
       let roleId = 0;
       for (i = 0; i < roleTitleArray.length; i++) {
         if (roleTitleArray[i] === answer.role) {
           roleId = i;
         }
       }
-      managerChoices();
       let managerId = 0;
-      for (i = 0; i < managerNameArray.length; i++) {
-        console.log(managerIdArray);
-        if (managerNameArray[i] === answer.manager) {
-          managerId =
-            managerIdArray[managerNameArray.indexOf(managerNameArray[i])];
+      if (answer.manager === "None") {
+        managerId = null;
+      } else {
+        for (i = 0; i < managerNameArray.length; i++) {
+          if (managerNameArray[i] === answer.manager) {
+            managerId =
+              managerIdArray[managerNameArray.indexOf(managerNameArray[i])];
+          }
         }
       }
-      console.log(managerId);
       connection.query(
         `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('${answer.first_name}', '${answer.last_name}', ${roleId}, ${managerId})`,
         (error, response) => {
           if (error) throw error;
-          console.log(response);
           console.log(`New Employee is successfully added`);
           mainMenu();
         }
@@ -200,11 +210,53 @@ const addEmployee = () => {
       throw new Error(error);
     });
 };
-
 /* === || Update Employee Role || === */
+const updateRole = () => {
+  inquirer
+    .prompt([
+      {
+        name: "reason",
+        type: "input",
+        message: "Please specify the reason for an update",
+      },
+      {
+        name: "empName",
+        type: "list",
+        message: "What is the name of the Employee?",
+        choices: empNameArray,
+      },
+      {
+        name: "newRole",
+        type: "list",
+        message: "Which role do you want to assign the selected employee?",
+        choices: roleChoices().title,
+      },
+    ])
+    .then(function (answer) {
+      let empId = 0;
+      for (i = 0; i < empNameArray.length; i++) {
+        if (empNameArray[i] === answer.empName) {
+          empId = empIdArray[empNameArray.indexOf(empNameArray[i])];
+        }
+      }
+      let roleId = 0;
+      for (i = 0; i < roleTitleArray.length; i++) {
+        if (roleTitleArray[i] === answer.newRole) {
+          roleId = roleIdArray[roleTitleArray.indexOf(roleTitleArray[i])];
+        }
+      }
+      connection.query(
+        `UPDATE employees SET role_id = ${roleId} WHERE employee_id = ${empId}`,
+        (error, response) => {
+          if (error) throw error;
+          console.log(`Employee role is successfully updated`);
+        }
+      );
+      mainMenu();
+    });
+};
 
 /* === || View All Roles || === */
-
 const viewAllRoles = () => {
   const sql = `
   SELECT  roles.role_id,
@@ -215,7 +267,6 @@ const viewAllRoles = () => {
   LEFT JOIN departments ON roles.department_id = departments.department_id;`;
   connection.query(sql, (error, response) => {
     if (error) throw error;
-    console.table(response);
     mainMenu();
   });
 };
@@ -242,7 +293,6 @@ const addRole = () => {
       },
     ])
     .then(function (answer) {
-      deptChoices();
       let deptId = 0;
       for (i = 0; i < deptNameArray.length; i++) {
         if (deptNameArray[i] === answer.department) {
